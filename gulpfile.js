b68f25f573531f69c131c6185c4f9deb3be6f1c7
Tommy <tommy@scribbledesign.co.uk>
2021-02-16 15:57:17 +0000

    add js compile and minify step

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,15 @@ const postcss = require('gulp-postcss'),
     precss = require('precss'),
     autoprefixer = require('autoprefixer'),
 	minify = require('cssnano');
+
+const babel = require('gulp-babel'),
+	uglify = require('gulp-uglify');
+
+const del = require('del');
 	
 const paths = {
 	css: '*.css',
+	js: '*.js',
 	dist: 'dist'
 }
 
@@ -25,4 +31,17 @@ function css() {
     .pipe(gulp.dest(paths.dist));
 };
 
+function js() {
+	return gulp.src(paths.js, '!gulpfile.js')
+	.pipe(babel({presets: ['@babel/preset-env']}))
+	.pipe(uglify())
+	.pipe(gulp.dest(paths.dist));
+}
+
+function clean() {
+	return del(paths.dist);
+}
+
 exports.css = css;
+exports.js = js;
+exports.clean = clean;
